dc0677e85f501e61c0cdd336a47c6275e5389e22
ibytex143 <saleyourdreamss@gmail.com>
2026-02-17 14:52:58 +0530

    Final payout + order integration fix

--- models/PayoutMethod.js
+++ models/PayoutMethod.js
@@ -4,7 +4,7 @@ const payoutSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "users",
       required: true,
     },
     type: {
